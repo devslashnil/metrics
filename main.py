0cdaf0b86e54bcc80ae464af4178a74b5e4ca83f
Mikhail Potapov <devslashnil@gmail.com>
2022-06-13 11:26:42 +0300

    2nd

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 from pydub import AudioSegment
 from pathlib import Path
+# pip install git+https://github.com/exeex/vocoder_eva.git
+from vocoder_eva.eval import eval_rmse_f0
+import librosa
 
-pathlist = Path("input_wavs_dir")
+def convert():
+    pathlist = Path("input_wavs_dir")
 
-for path in pathlist:
-    filename = path.stem
-    AudioSegment.from_wav("/input/{}.wav".format(filename))\
-        .export("/output_dir/{}.mp3".format(filename), format="mp3")
+    for path in pathlist:
+        filename = path.stem
+
+        AudioSegment.from_wav("/input/{}.wav".format(filename)) \
+            .export("/output_dir/{}.mp4}.".format(filename), format="mp4")
+        AudioSegment.from_wav("/input/{}.wav".format(filename))\
+            .export("/output_dir/{}.mp3".format(filename), format="mp3")
+        AudioSegment.from_wav("/input/{}.wav".format(filename)) \
+            .export("/output_dir/{}.ogg".format(filename), format="ogg")
+        AudioSegment.from_wav("/input/{}.wav".format(filename)) \
+            .export("/output_dir/{}.flac".format(filename), format="flac")
+        AudioSegment.from_wav("/input/{}.wav".format(filename)) \
+            .export("/output_dir/{}.webm".format(filename), format="webm")
+
+def score():
+    pathlist = Path("input_wavs_dir")
+
+    for path in pathlist:
+        filename = path.stem
+        ext = path.suffix
+
+        if ext == ".mp4":
+            continue
+        if ext == ".mp3":
+            continue
+        if ext == ".ogg":
+            continue
+        if ext == ".flac":
+            continue
+        if ext == ".webm":
+            continue
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
